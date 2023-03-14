0453a36464e95448cf8f0f34e94b7a6ea3d97370
Tim Simms <tsimms@gmail.com>
2023-03-14 17:49:51 -0500

    task 4

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import Input from './components/general/Input';
-import SelectInput from './components/general/SelectInput';
-import Button from './components/general/Button';
-import SearchBar from './components/general/SearchBar';
-import { faUser } from '@fortawesome/free-solid-svg-icons';
-import Dashboard from './components/dashboard/Dashboard';
-import Authentication from './components/authentication/Authentication';
+// import Input from './components/general/Input';
+// import SelectInput from './components/general/SelectInput';
+// import Button from './components/general/Button';
+// import SearchBar from './components/general/SearchBar';
+// import { faUser } from '@fortawesome/free-solid-svg-icons';
+// import Dashboard from './components/dashboard/Dashboard';
+// import Authentication from './components/authentication/Authentication';
+import Login from './routes/auth/Login';
+import Authentication from './routes/auth/Authentication';
+
 import './App.css';
 
 function App() {
@@ -41,22 +44,8 @@ function App() {
 
   return (
     <div className="App">
-      {isLoggedIn
-        ? <Dashboard username={userUsername} />
-        : <Authentication />
-      }
-{/* 
-      <Input label="Username" type="text" className="whiteInput" setValue={setValue} placeholder="name" icon={faUser}/>
-      <Input label="Min Date" type="number" className="blackInput" setValue={setValue} icon={"user"} />
-
-      <SelectInput label="Select" className="whiteSelect" setValue={setValue} options={[{ value: "1", label: "One" }, { value: "2", label: "Two" }, { value: "3", label: "Three" }]} />
-      <SelectInput label="Select" className="blackSelect" setValue={setValue} options={[{ value: "1", label: "One" }, { value: "2", label: "Two" }, { value: "3", label: "Three" }]} />
-
-      <Button label="Load More..." className="squareButton" />
-      <Button label="Drama" className="roundButton" />
-
-      <SearchBar title="Search" className="lightSearch" setTitle={setTitle} />
-      <SearchBar title="Search" className="darkSearch" setTitle={setTitle} /> */}
+      {/* <Login setValue={setValue} setTitle={setTitle} /> */}
+      <Authentication setIsLoggedIn={setIsLoggedIn} setUserUsername={setUserUsername} />
     </div>
   );
 }
