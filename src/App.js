4288b95b526b53358f1a7571413a40b6b8e6ae8e
Tim Simms <tsimms@gmail.com>
2023-03-14 15:24:24 -0500

    task 3

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
-// import { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Input from './components/general/Input';
+import SelectInput from './components/general/SelectInput';
+import Button from './components/general/Button';
+import SearchBar from './components/general/SearchBar';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+import Dashboard from './components/dashboard/Dashboard';
+import Authentication from './components/authentication/Authentication';
 import './App.css';
-// import Button from './components/general/Button.js';
-import Input from './components/general/Input.js';
-import SearchBar from './components/general/SearchBar.js';
-import SelectInput from './components/general/SelectInput.js';
-import { faUser, faSearch } from '@fortawesome/free-solid-svg-icons'
-import Button from './components/general/Button.js';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userUsername, setUserUsername] = useState('');
 
+  useEffect(() => {
+    const accessToken = localStorage.getItem('accessToken');
+    if (!accessToken) return;
+
+    axios.post('http://localhost:8000/api/auth/', null, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    })
+    .then(response => {
+      setIsLoggedIn(true);
+      setUserUsername(response.data.username);
+    })
+    .catch(error => {
+      console.error(error);
+    });
+  }, []);
 
   function setValue(e) {
     console.log(e.target.value);
@@ -20,9 +41,22 @@ function App() {
 
   return (
     <div className="App">
+      {isLoggedIn
+        ? <Dashboard username={userUsername} />
+        : <Authentication />
+      }
+{/* 
+      <Input label="Username" type="text" className="whiteInput" setValue={setValue} placeholder="name" icon={faUser}/>
+      <Input label="Min Date" type="number" className="blackInput" setValue={setValue} icon={"user"} />
+
+      <SelectInput label="Select" className="whiteSelect" setValue={setValue} options={[{ value: "1", label: "One" }, { value: "2", label: "Two" }, { value: "3", label: "Three" }]} />
+      <SelectInput label="Select" className="blackSelect" setValue={setValue} options={[{ value: "1", label: "One" }, { value: "2", label: "Two" }, { value: "3", label: "Three" }]} />
+
+      <Button label="Load More..." className="squareButton" />
+      <Button label="Drama" className="roundButton" />
 
       <SearchBar title="Search" className="lightSearch" setTitle={setTitle} />
-      <SearchBar title="Search" className="darkSearch" setTitle={setTitle} />
+      <SearchBar title="Search" className="darkSearch" setTitle={setTitle} /> */}
     </div>
   );
 }
